e9f77ef165cb9fb1e2d083c9d325b65f9f8a7afe
reloaded <azizur.rahman.bu@gmail.com>
2018-07-15 16:36:49 +0600

    initial

---
+++ app/src/main/java/asthq/net/monofilterimage/MainActivity.java
@@ -0,0 +1,89 @@
+package asthq.net.monofilterimage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Bitmap bitmap;
+
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.image);
+        imageView = findViewById(R.id.ivImage);
+        //Bitmap bmp=toGrayscale(bitmap);
+        Bitmap bmp=createBlackAndWhite(bitmap);
+        imageView.setImageBitmap(bmp);
+
+
+    }
+
+    public Bitmap toGrayscale(Bitmap bmpOriginal)
+    {
+        int width, height;
+        height = bmpOriginal.getHeight();
+        width = bmpOriginal.getWidth();
+
+        Bitmap bmpGrayscale = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+        Canvas c = new Canvas(bmpGrayscale);
+        Paint paint = new Paint();
+        ColorMatrix cm = new ColorMatrix();
+        cm.setSaturation(0);
+        ColorMatrixColorFilter f = new ColorMatrixColorFilter(cm);
+        paint.setColorFilter(f);
+        c.drawBitmap(bmpOriginal, 0, 0, paint);
+        return bmpGrayscale;
+    }
+
+    public static Bitmap createBlackAndWhite(Bitmap src) {
+        int width =  (src.getWidth());
+        int height =(src.getHeight());
+        // create output bitmap
+        Bitmap bmOut = Bitmap.createBitmap(width, height, src.getConfig());
+        /*Bitmap bmOut = Bitmap.createScaledBitmap(src,(int)(width*.5),(int)(height*.5),true);
+        width=bmOut.getWidth();
+        height=bmOut.getHeight();*/
+        // color information
+        int A, R, G, B;
+        int pixel;
+
+        // scan through all pixels
+        for (int x = 0; x < width; ++x) {
+            for (int y = 0; y < height; ++y) {
+                // get pixel color
+                pixel = src.getPixel(x, y);
+                A = Color.alpha(pixel);
+                R = Color.red(pixel);
+                G = Color.green(pixel);
+                B = Color.blue(pixel);
+                int gray = (int) (0.2989 * R + 0.5870 * G + 0.1140 * B);
+
+                // use 128 as threshold, above -> white, below -> black
+                if (gray > 128)
+                    gray = 255;
+                else
+                    gray = 0;
+                // set new pixel color to output bitmap
+                bmOut.setPixel(x, y, Color.argb(A, gray, gray, gray));
+            }
+        }
+        return bmOut;
+    }
+}
